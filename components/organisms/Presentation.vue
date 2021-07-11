4ecb0a9a2035818a4b1dd7e58d6d059d57663c4b
Lucas Silva Dantas Abrantes <lucasabrantes002@gmail.com>
2021-07-11 20:12:26 -0300

    Pagina sobre

---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="presentation">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias inventore error eligendi quae in officiis neque voluptate id harum ducimus veritatis, et quas quo sapiente esse ad commodi ab sit!
+    </p>
+    <img src="@/assets/img/presentation.svg" alt="Apresentação do Ibook">
+  </div>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+
+  })
+</script>
+
+<style lang="scss" scoped>
+  .presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  p {
+    line-height: 26px;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
